3de774fffc12b33d4b9d6054d893bb802a3eb10d
Loot72 <whghrms@gmail.com>
2026-06-09 12:58:20 +0900

    Tailwind CSS 최신 문법으로 수정 및 디자인 복구

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/auto-grader/', // ⭐️ 선생님의 깃허브 저장소 이름을 적어줍니다.
 })
