74c0818f1900ca640c1a3f3ffec79e96915f83ad
Loot72 <whghrms@gmail.com>
2026-06-09 14:01:09 +0900

    Vite 및 Tailwind v4 최신 설정 반영

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/auto-grader/', // ⭐️ 선생님의 깃허브 저장소 이름을 적어줍니다.
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  base: '/auto-grader/',
 })
